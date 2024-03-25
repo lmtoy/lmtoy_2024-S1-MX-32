4efa4d16cccaf9d41ad947f19484cd4e0bc2b072
Peter Teuben <teuben@gmail.com>
2024-03-25 22:29:58 +0000

    only one partial map

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# 2023-S1-US-18
+# 2024-S1-MX-32
 
-this project ....
+400MHz (4k channels), only one IF
+
+ValueError: could not convert string to float: '115.31297436490603,88.62984729578281'
+
+
+numbands=1 didn't work
+
--- mk_runs.py
+++ mk_runs.py
@@ -6,24 +6,25 @@ import sys
 
 from lmtoy import runs
 
-project="2023-S1-US-00"
+project="2024-S1-MX-32"
 
 #        obsnums per source (make it negative if not added to the final combination)
 on = {}
-on['foo'] = [ ]
+on["G38.60"] =  [ 112902,]
 
 
 #        common parameters per source on the first dryrun (run1a, run2a)
 pars1 = {}
-pars1['foo']   = ""
+pars1["G38.60"] = "dv=20 dw=20"
+
 
 #        common parameters per source on subsequent runs (run1b, run2b), e.g. bank=0 for WARES
 pars2 = {}
-pars2['foo']   = "srdp=1 admit=0"
+pars2["G38.60"] = "pix_list=-13"
 
 #        common parameters per source on subsequent runs (run1c, run2c), e.g. bank=1 for WARES
 pars3 = {}
-pars3['foo']   = "srdp=1 admit=0"
+pars3["G38.60"] = ""
 
 if __name__ == '__main__':    
     runs.mk_runs(project, on, pars1, pars2, pars3, sys.argv)
